d18510c69626e454d20583aa2bf4555143baeaae
peneshri <pennyshriver31124@gmail.com>
2024-08-02 20:09:17 +0000

    message

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -3,8 +3,18 @@
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring>
 
 int main(){
+    const size_t LENGTH1 = 25;
+
+    char array_str1[LENGTH1] = "Hey guys! ";
+    char array_str2[] = "What's up?";
+
+    std::string std_str1 = "Hi everybody! ";
+    std::string std_str2 = "How's it going?";
+    
     
     std::cout << std::endl << std::endl;
     return (0);
